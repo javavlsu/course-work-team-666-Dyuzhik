4f660a5b2936b1eeb927436a455dd8ee7916a788
AspirinIT <51916515+AspirinIT@users.noreply.github.com>
2023-01-18 01:31:36 +0300

    sql script

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -41,9 +41,38 @@ values (1, 1),
        (1, 3),
        (2, 1);
 
+create table Specialization
+(
+    id   int          not null auto_increment primary key,
+    name varchar(255) not null
+);
+
 create table Service
 (
-    id int not null auto_increment,
-    name varchar(255) not null,
+    id                int          not null auto_increment,
+    name              varchar(255) not null,
+    specialization_id int          not null,
+    foreign key (specialization_id) references Specialization (id),
     primary key (id)
+);
+
+create table Barber
+(
+    id                int          not null auto_increment primary key,
+    user_id           int          not null,
+    position          varchar(255) not null,
+    specialization_id int          not null,
+    foreign key (user_id) references user (id),
+    foreign key (specialization_id) references Specialization (id)
+);
+
+create table Record
+(
+    id        int          not null auto_increment primary key,
+    client_id int          not null,
+    barber_id int          not null,
+    date      datetime     not null,
+    status    varchar(255) not null,
+    foreign key (client_id) references user (id),
+    foreign key (barber_id) references user (id)
 )
